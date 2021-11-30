78891c676b9895b2b326a86b42734648121c84f8
bobubear <jedsa.pd@gmail.com>
2021-11-30 17:38:29 +0700

    Lab7_3 Complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+int adiff(int a, int b){
+  int c = a - b;
+  if(c < 0){
+    c = -c;
+  }
+  c = c%360;
+  if(c > 180){
+    c = 360 - c;
+  }
+  return c;
+}
 
 int main(){
   cout << adiff(180,270);
